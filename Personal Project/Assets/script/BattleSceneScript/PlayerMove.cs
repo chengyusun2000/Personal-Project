55d010e838063fe8624c61dc6fd9b2f7c0836683
chengyu <chengyusun2000@gmail.com>
2020-04-17 01:23:01 -0400

    player movement in battle scene and camera follow

---
+++ Personal Project/Assets/script/BattleSceneScript/PlayerMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 5f;
+    public Vector2 Movement;
+    private Rigidbody2D PlayerRigi;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerRigi = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement.x = Input.GetAxis("Horizontal");
+        Movement.y = Input.GetAxis("Vertical");
+       if(Input.GetButtonDown("Jump"))
+        {
+            PlayerRigi.AddForce(Movement);
+        }
+    }
+    private void FixedUpdate()
+    {
+        PlayerRigi.MovePosition(Movement * speed + PlayerRigi.position);
+
+
+   
+    }
+}
--- Personal Project/Assets/script/CameraFollow.cs
+++ Personal Project/Assets/script/CameraFollow.cs
@@ -5,16 +5,29 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Camera camera;
+    public Vector3 velocity;
     public Transform player;
+    public float smoothTime;
+    private float x;
+    private float y;
     // Start is called before the first frame update
     void Start()
     {
+        camera = GameObject.FindObjectOfType<Camera>();
         player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+         
+
+    }
+    private void FixedUpdate()
+    {
+        x = Mathf.SmoothDamp(transform.position.x, player.position.x, ref velocity.x, smoothTime);
+        y = Mathf.SmoothDamp(transform.position.y, player.position.y, ref velocity.y, smoothTime);
+        //transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+        transform.position = new Vector3(x, y, transform.position.z);
     }
 }
