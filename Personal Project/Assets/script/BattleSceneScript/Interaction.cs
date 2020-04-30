49bbd323ee2f2cb44e7cb65745187c9a22f32ca3
chengyu <chengyusun2000@gmail.com>
2020-04-30 11:17:07 -0400

    new

---
+++ Personal Project/Assets/script/BattleSceneScript/Interaction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    public LayerMask rayMask;
+    public RaycastHit2D Hit2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(transform.position, transform.right);
+        Hit2D= Physics2D.Raycast(transform.position, transform.right, rayMask);
+        if(Hit2D.transform.tag=="Box")
+        {
+
+        }
+    }
+}
