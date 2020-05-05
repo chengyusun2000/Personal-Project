851837bc8d5e07af893ad369ef8515c7cc579c07
chengyu <chengyusun2000@gmail.com>
2020-05-05 00:20:32 -0400

    terrain instantiate test

---
+++ Assets/script/TerrainInsScript/Ins.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ins : MonoBehaviour
+{
+    [SerializeField] private GameObject[] Prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(Prefab.Length);
+        Instantiate(Prefab[Random.Range(0,Prefab.Length)], transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
