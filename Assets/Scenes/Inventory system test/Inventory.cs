175da2169db0a0fbfc691c8f3ee6f0dbd874b879
chengyu <chengyusun2000@gmail.com>
2020-05-09 23:54:00 -0400

    update inventory system
    now player can add an item to inventory with a sorted order when the item is picked up

--- Assets/Scenes/Inventory system test/Inventory.cs
+++ Assets/Scenes/Inventory system test/Inventory.cs
@@ -11,9 +11,11 @@ public class Inventory : MonoBehaviour
     public Image Parent;
     public float x;
     public float y;
-    public Food food;
+    public ItemObj food;
+    public ItemObj Potion;
     public int Width=12;
     public int length = 8;
+    [SerializeField] private bool PickedUp = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,10 +29,21 @@ public class Inventory : MonoBehaviour
             {
                 slots[i, o] = new Slots(false, (x + 80 * (i + 1)), (y + 80 * (o + 1)));
                 Sslots.Add(new Slots(false, (x + 80 * (i + 1)), (y + 80 * (o + 1))));
-                Instantiate(test, new Vector3((x + 80 * (i + 1)), (y + 80 * (o + 1))), Quaternion.identity, Parent.transform);
+                //Instantiate(test, new Vector3((x + 80 * (i + 1)), (y + 80 * (o + 1))), Quaternion.identity, Parent.transform);
             }
         }
-        AddI(2, 2, food);
+        //AddItem(6, 6, food);
+        PickUpObject(food);
+        PickUpObject(food);
+        PickUpObject(Potion);
+        PickUpObject(food);
+        PickUpObject(Potion);
+        PickUpObject(food);
+        PickUpObject(food);
+        PickUpObject(food);
+        PickUpObject(Potion);
+        PickUpObject(food);
+        PickUpObject(Potion);
         //AddItem(x + 160, y + 160, food);
         //AddItem(x + 160, y + 160, food);
 
@@ -41,20 +54,86 @@ public class Inventory : MonoBehaviour
     {
        
     }
-    public void AddI(int x,int y,Items items)
-    {
+    //public void AddI(int x,int y,ItemObj items)
+    //{
 
-        if ((x == 0 || y == 0) && (items.height > 1 || items.width > 1))
+    //    if ((x == 0 || y == 0) && (items.height > 1 || items.width > 1))
+    //    {
+    //        Debug.Log("oc");
+    //        return;
+    //    }
+    //    else
+    //    {
+
+    //        for (int CheckX = x - items.height / 2; CheckX <= x + items.height / 2; CheckX++)
+    //        {
+    //            for (int CheckY = y - items.width / 2; CheckY <= y + items.width / 2; CheckY++)
+    //            {
+    //                if (slots[CheckX, CheckY].Occupied)
+    //                {
+    //                    Debug.Log("occ");
+    //                    return;
+    //                }
+    //                else
+    //                {
+    //                    Debug.Log("yes");
+    //                }
+
+    //            }
+    //        }
+            
+    //        float PositionX = (float)(69.19995 + 80 * x);
+    //        float PositionY = (float)(262.7 + 80 * y);
+    //        Instantiate(items.image, new Vector3(PositionX, PositionY, 0), Quaternion.identity, Parent.transform);
+
+    //        for (int CheckX = x - items.height / 2; CheckX<= x + items.height / 2; CheckX++)
+    //        {
+    //            for (int CheckY = y - items.width / 2; CheckY <= y + items.width / 2; CheckY++)
+    //            {
+    //                Debug.Log(CheckX + " " + CheckY);
+    //                slots[CheckX, CheckY].Occupied = true;
+
+    //            }
+    //        }
+    //    }
+    //}
+    public void PickUpObject(ItemObj item)
+    {
+        
+        for (int i = 0; i < 12; i++)
+        {
+            for (int o = 0; o < 8; o++)
+            {
+                if(!PickedUp)
+                {
+                    if (!slots[i, o].Occupied)
+                    {
+                        AddItem(i, o, item);
+
+                    }
+                }
+               
+            }
+        }
+        PickedUp = false;
+
+
+    }
+
+    public void AddItem(int x,int y,ItemObj items)
+    {
+        if ((Width-x<items.width)||(length-y<items.height))
         {
             Debug.Log("oc");
+            PickedUp = false;
             return;
         }
         else
         {
 
-            for (int CheckX = x - items.height / 2; CheckX <= x + items.height / 2; CheckX++)
+            for (int CheckX = x; CheckX < items.width+x; CheckX++)
             {
-                for (int CheckY = y - items.width / 2; CheckY <= y + items.width / 2; CheckY++)
+                for (int CheckY = y ; CheckY < items.height+y; CheckY++)
                 {
                     if (slots[CheckX, CheckY].Occupied)
                     {
@@ -68,13 +147,41 @@ public class Inventory : MonoBehaviour
 
                 }
             }
-            float PositionX = (float)(69.19995 + 80 * x);
-            float PositionY = (float)(262.7 + 80 * y);
-            Instantiate(items.Image, new Vector3(PositionX, PositionY, 0), Quaternion.identity, Parent.transform);
-
-            for (int CheckX = x - items.height / 2; CheckX<= x + items.height / 2; CheckX++)
+            float FloatWidth = items.width;
+            float FloatHeight = items.height;
+            float PositionX;
+            float PositionY;
+            if (OddOrEven(items.width)==0)
             {
-                for (int CheckY = y - items.width / 2; CheckY <= y + items.width / 2; CheckY++)
+                 PositionX = (float)(69.19995 + 80 * (FloatWidth / 4) + 80 * x);
+            }
+            else
+            {
+                PositionX = (float)(69.19995 + 80 * ((FloatWidth-1) / 2) + 80 * x);
+            }
+
+
+            if(OddOrEven(items.height)==0)
+            {
+                 PositionY = (float)(262.7 + 80 * (FloatHeight / 4) + 80 * y);
+            }
+            else
+            {
+                 PositionY = (float)(262.7 + 80 * ((FloatHeight-1) / 2) + 80 * y);
+            }
+
+            
+            
+            Debug.Log("divide" + FloatWidth / 2);
+            
+            
+            
+            Instantiate(items.image, new Vector3(PositionX, PositionY, 0), Quaternion.identity, Parent.transform);
+            PickedUp = true;
+            Debug.Log(PositionX + " " + PositionY);
+            for (int CheckX = x; CheckX < items.width + x; CheckX++)
+            {
+                for (int CheckY = y; CheckY < items.height + y; CheckY++)
                 {
                     Debug.Log(CheckX + " " + CheckY);
                     slots[CheckX, CheckY].Occupied = true;
@@ -82,45 +189,17 @@ public class Inventory : MonoBehaviour
                 }
             }
         }
+
     }
-
-
-    public void AddItem(float x,float y,Items items)
+    public int OddOrEven(int Number)
     {
-        Debug.Log(x);
-        Debug.Log(x - 80 * (items.width / 2));
-        Debug.Log(80 * (items.width / 2) + x);
-        for(float CheckX=(x-80*(items.width/2));CheckX<=80* (items.width / 2) + x;CheckX=CheckX+80)
+        if(Number%2==0)
         {
-            for (float CheckY = (y - 80 * (items.height / 2)); CheckY <= 80 * (items.height/2)+y; CheckY = CheckY + 80)
-            {
-                int i=((int)(CheckX-69.19995)/80);
-                int o= ((int)(CheckY - 262.7) / 80);
-                Debug.Log(i+","+o);
-                if (slots[i, o].Occupied)
-                {
-                    Debug.Log("occ");
-                    return;
-                }
-                else
-                {
-                    Debug.Log("yes");
-                }
-
-            }
+            return 0;
         }
-        Instantiate(items.Image, new Vector3(x, y, 0), Quaternion.identity, Parent.transform);
-        for (float CheckX = (x - 80 * (items.width / 2)); CheckX <= 80 * (items.width / 2) + x; CheckX = CheckX + 80)
+        else
         {
-            for (float CheckY = (y - 80 * (items.height / 2)); CheckY <= 80 * (items.height / 2) + y; CheckY = CheckY + 80)
-            {
-                int i = ((int)(CheckX - 69.19995) / 80);
-                int o = ((int)(CheckY - 262.7) / 80);
-                Debug.Log(i + "," + o);
-                slots[i, o].Occupied = true;
-
-            }
+            return 1;
         }
-
     }
 }
---
+++ Assets/Scenes/Inventory system test/ScriptableObj/Script/FoodObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "FoodObject", menuName = "InventorySystem/Item/Food")]
+public class FoodObject : ItemObj
+{
+    public int HealthRestore;
+    public int ManaRestore;
+    public int HungerRestore;
+    public void Awake()
+    {
+        type = ItemType.Food;
+        
+
+    }
+}
