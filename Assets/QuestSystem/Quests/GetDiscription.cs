add75f306dedd63f001b3f26fc19ab85fa042e19
chengyu <chengyusun2000@gmail.com>
2020-05-21 11:23:28 -0400

    quest system update
    now we have a simple panel that can display quests name and discription of the quest

---
+++ Assets/QuestSystem/Quests/GetDiscription.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GetDiscription : MonoBehaviour
+{
+    [SerializeField]private QuestBase Quest;
+    [SerializeField] private Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GameObject.FindGameObjectWithTag("Canvas").transform.Find("Quests").Find("Discription").Find("Viewport").Find("Content").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void DisPlayDiscription()
+    {
+        text.text = Quest.GetCurrentEvent(Quest.FindFirstEvent())[0].Discription;
+    }
+    public void GetQuest(QuestBase quest)
+    {
+        Quest = quest;
+    }
+}
