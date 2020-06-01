03f31f6fd5b46cd1c9180e773f783908ee66629b
Boren <56322187+jopjiang@users.noreply.github.com>
2020-06-01 10:55:58 -0400

    Menu 0.1
    Update to zero version

--- Assets/script/Menu/LoadScene.cs
+++ Assets/script/Menu/LoadScene.cs
@@ -7,21 +7,8 @@ public class LoadScene : MonoBehaviour
 {
     private AsyncOperation asy;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void BtnLoadScene(string s)
     {
-        //Cursor.visible = true;
         if (asy == null)
         {
             asy = SceneManager.LoadSceneAsync(s);
