35a111e7b6884c38ef9d4cf1fd5a469e5ffb0e8d
Boren <56322187+jopjiang@users.noreply.github.com>
2020-06-01 10:22:43 -0400

    Menu V0
    Simple menu
    Zero edition

---
+++ Assets/script/Menu/LoadScene.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    private AsyncOperation asy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BtnLoadScene(string s)
+    {
+        //Cursor.visible = true;
+        if (asy == null)
+        {
+            asy = SceneManager.LoadSceneAsync(s);
+            asy.allowSceneActivation = true;
+            Time.timeScale = 1f;
+        }
+    }
+}
