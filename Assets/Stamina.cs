c4d61c2781f4fa39fa70f995725e8029514d0b56
Boren <56322187+jopjiang@users.noreply.github.com>
2020-06-07 20:00:50 -0400

    PlayerUI
    HP, MANA, EXP && Stiaima

---
+++ Assets/Stamina.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stamina : MonoBehaviour
+{
+    public Slider foodAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Test"))
+        {
+            Debug.Log("Stamina" + other.name);
+            foodAmount.value -= 8;
+        }
+    }
+}
