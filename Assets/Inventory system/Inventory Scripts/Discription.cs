486325eccfeac180fa6c58f04bf99d5d1bb68b5f
chengyu <chengyusun2000@gmail.com>
2020-06-08 00:14:57 -0400

    implementation of use button and discription panel

---
+++ Assets/Inventory system/Inventory Scripts/Discription.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Discription : MonoBehaviour
+{
+    private ItemObj item;
+    private Text NameText;
+    private Text TypeText;
+    private Text DiscriptionText;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        NameText = transform.Find("Name").GetComponent<Text>();
+        TypeText = transform.Find("Type").GetComponent<Text>();
+        DiscriptionText = transform.Find("Discription").GetComponent<Text>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NameText.text = item.name;
+        TypeText.text = item.type.ToString();
+        DiscriptionText.text = item.discription;
+    }
+
+    public void SetItem(ItemObj itemObj)
+    {
+        item = itemObj;
+    }
+}
