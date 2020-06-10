938deb6de1b8919b5e49782fe60379191d9ab0e7
chengyu <chengyusun2000@gmail.com>
2020-06-10 17:14:26 -0400

    test load scene
    when player triggers an event and switch to another scene, the system does not need to destory the main map, instead, the map is kept, as a result, the data does not need to be save every single time  when events are triggered

---
+++ Assets/SceneManager/TestScene.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class TestScene : MonoBehaviour
+{
+    private void Awake()
+    {
+        if (SceneManager.GetSceneByName("MainMap").isLoaded)
+        {
+            Scene scene = SceneManager.GetSceneByName("MainMap");
+            if (scene != null)
+            {
+                foreach (GameObject gameObject in scene.GetRootGameObjects())
+                {
+                    gameObject.SetActive(false);
+                }
+            }
+        }
+        StartCoroutine(SetActive(SceneManager.GetSceneByName("TestInsScene")));
+    }
+    private void OnEnable()
+    {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public IEnumerator SetActive(Scene scene)
+    {
+        int i = 0;
+        while (i == 0)
+        {
+            i++;
+            yield return null;
+        }
+        SceneManager.SetActiveScene(scene);
+        yield break;
+    }
+
+    public void load()
+    {
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName("Mainmap"));
+        if (SceneManager.GetSceneByName("MainMap").isLoaded)
+        {
+            Scene scene = SceneManager.GetSceneByName("MainMap");
+            if (scene != null)
+            {
+                foreach (GameObject gameObject in scene.GetRootGameObjects())
+                {
+                    if(gameObject.tag!="DontEnable")
+                    {
+                        gameObject.SetActive(true);
+                    }
+                    
+                }
+            }
+        }
+        SceneManager.UnloadSceneAsync("TestInsScene");
+    }
+}
